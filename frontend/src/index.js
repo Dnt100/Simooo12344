5b1eaf323b67f8dd51793705d162c5a3a20cd859
Simooo12344 <mohamed.oudrhiri@edu.ece.fr>
2021-11-21 10:12:03 +0100

    go

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { Provider } from 'react-redux'
+
+import rootReducer from './redux/reducers'
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/theme.css'
+import './assets/css/index.css'
+
+import Layout from './components/layout/Layout'
+
+const store = createStore(
+  rootReducer
+)
+
+document.title = 'Raouf Simo'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
